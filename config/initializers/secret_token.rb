2a2ab00514a4437721413bfa0bb93420e1005728
Katherine G. Pe <katz@bridgeutopiaweb.com>
2012-09-08 15:08:21 +0800

    Rails 3.2.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sleep::Application.config.secret_token = '687a82eab45625f856af7a82069f22332fb027eea1a0246ca40285bfde9ae1839f044aef11b634ed917b5678a3e9d6b4e1cb09f9460462f91e2c74003eca6237'
